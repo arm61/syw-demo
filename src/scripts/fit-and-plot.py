f641a87dfe89c3a99f4cba34c3ab5c17bfe6d916
Andrew R. McCluskey <andrew.mccluskey@bristol.ac.uk>
2024-03-20 14:50:43 +0000

    Change the figure colors

--- src/scripts/fit-and-plot.py
+++ src/scripts/fit-and-plot.py
@@ -11,8 +11,8 @@ beta = X.T @ W @ y @ np.linalg.inv(X.T @ W @ X)
 intercept, gradient = beta
 
 fig, ax = plt.subplots(figsize=(6, 4))
-ax.errorbar(x, y, yerr, ls='', color='#5CDB94')
-ax.plot(x, gradient * x + intercept, '-', 'k')
+ax.errorbar(x, y, yerr, 'k.')
+ax.plot(x, gradient * x + intercept, 'k-')
 ax.set_ylabel('$y$')
 ax.set_xlabel('$x$')
 fig.savefig(paths.figures / 'my_plot.pdf')
